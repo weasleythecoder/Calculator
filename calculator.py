e28c0614b5fc45666a39a170ea11abc526972461
bedboyxxx <elshad.b.2016@gmail.com>
2022-07-14 13:10:13 +0400

    men eledim

---
+++ calculator.py
@@ -0,0 +1,19 @@
+def calculator(number1, number2, operation):
+    if operation == "+":
+        print(number1 + number2)
+    if operation == "-":
+        print(number1 - number2)
+    if operation == "*":
+        print(number1 * number2)
+    if operation == "/":
+        print(number1 / number2)
+
+
+print("Enter Number 1:")
+a = int(input())
+print("Enter Number 2:")
+b = int(input())
+print("What you want to do ?")
+c = str(input())
+print("This is what you got: ")
+calculator(a, b, c)
